9ec543c62eba6d7b7e9a80a7793ab24325f9f922
Jimin <jimin.stella@gmail.com>
2018-10-09 11:01:09 -0700

    UDP Server/Client
    Signed-off-by: Jimin <jimin.stella@gmail.com>

--- clientUDP.py
+++ clientUDP.py
@@ -1,9 +1,43 @@
 import socket
+import sys
+import struct
 
-UDP_IP_ADDRESS = "127.0.0.1"
-UDP_PORT_NO = 12345
-Message = "Practice".encode("utf-8")
+# Variables from the arguments
+# print(sys.argv) -> check the arguments stored as string type, see the single quote
+host = sys.argv[1] # string
+port = int(sys.argv[2]) # int
+op = sys.argv[3]
+cnt = len(sys.argv[4:])
+print(host, port, op, cnt)
 
-clientS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-clientS.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT_NO))
+# Building the datagram packet sent to the server, the packet is the type of bytearray()
+packet = bytearray()
 
+# First byte
+if op == '+':
+    packet.append(1)
+elif op == '-':
+    packet.append(2)
+elif op == '*':
+    packet.append(4)
+
+# Second byte
+packet.append(cnt)
+
+# Third byte
+for i in range(4, cnt+3, 2):
+    packet.append(int(sys.argv[i]) << 4 | int(sys.argv[i+1]))
+
+if cnt % 2:
+    packet.append(int(sys.argv[i+2]) << 4)
+
+"""
+for i in range(0, len(packet), 1):
+    print(hex(packet[i]), end = ' ',flush=True)
+"""
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendto(packet, (host, port))
+
+result = s.recvfrom(1024)
+print(struct.unpack('!i', result[0])[0])
--- serverUDP.py
+++ serverUDP.py
@@ -1,11 +1,46 @@
 import socket
+import struct
 
-UDP_IP_ADDRESS = "127.0.0.1"
-UDP_PORT_NO = 12345
+def operator(l, op, r):
+    if op == 1:
+        l += r
+    elif op == 2:
+        l -= r
+    elif op == 4:
+        l *= r
 
-serverS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverS.bind( (UDP_IP_ADDRESS, UDP_PORT_NO) )
+    return l
+
+host = "127.0.0.1"
+port = 12345
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
 
 while True:
-    data, addr = serverS.recvfrom(1024)
-    print("Message: " + data.decode("utf-8"))
+    packet, addr = s.recvfrom(1024)
+
+    for i in range(0, len(packet), 1):
+        print(hex(packet[i]), end = ' ', flush=True)
+    print()
+
+    op = packet[0]
+    cnt = packet[1]
+    data = packet[2:]
+
+    result = data[0] >> 4
+    v = data[0] & 0x0f
+    result = operator(result, op, v)
+
+    for i in range(1, int(cnt / 2), 1):
+        result = operator(result, op, data[i] >> 4)
+        result = operator(result, op, data[i] & 0x0f)
+    print("i ?", i)
+    if cnt % 2:
+        result = operator(result, op, data[i + 1] >> 4)
+    print(result)
+
+    print(struct.pack('!i', result))
+    s.sendto(struct.pack('!i', result), addr)
+
+
